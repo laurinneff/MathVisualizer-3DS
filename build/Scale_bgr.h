e42738670f1158a14e2b6e66fad97da8c5e2ab7e
Laurin Neff <laurinneff@gmail.com>
2018-09-09 15:16:49 +0200

    .gitignore fix

--- build/Scale_bgr.h
+++
@@ -1,3 +0,0 @@
-extern const u8 Scale_bgr_end[];
-extern const u8 Scale_bgr[];
-extern const u32 Scale_bgr_size;
--- build/brew_bgr.h
+++
@@ -1,3 +0,0 @@
-extern const u8 brew_bgr_end[];
-extern const u8 brew_bgr[];
-extern const u32 brew_bgr_size;
